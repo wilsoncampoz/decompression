4cfe2b37e9ad69d2d5aba447528de43a3c3ee1a5
woliveiras <w.oliveira542@gmail.com>
2015-08-15 08:02:47 -0300

    Update app

--- sort.io/src/js/app.js
+++ sort.io/src/js/app.js
@@ -5,11 +5,11 @@ function raffle(){
 
   if(shot.length > 2) {
     resultBox.innerHTML = " ";
-    resultBox.innerHTML += "<p>" + winn + "</p>";
+    resultBox.innerHTML += "<p class='success'>Vencedor: " + winn + "</p>";
   } else if(shot[0] == ""){
-    resultBox.innerHTML = "<p>Você esqueceu de digitar os valores. ;)</p>";
+    resultBox.innerHTML = "<p class='alert'>Você esqueceu de digitar os valores. ;)</p>";
   } else {
-    resultBox.innerHTML = "<p>Coloque mais de um valor separado por vírgulas.</p>";
+    resultBox.innerHTML = "<p class='alert'>Coloque mais de um valor separado por vírgulas.</p>";
   };
 };
 
