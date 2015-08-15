8b96782f8c5ff62332eb97ae4e16c914ec9936ca
woliveiras <w.oliveira542@gmail.com>
2015-08-15 07:30:01 -0300

    Update app

--- sort.io/src/js/app.js
+++ sort.io/src/js/app.js
@@ -1,11 +1,16 @@
 function raffle(){
-  var box = document.getElementById("sortBox"),
-      resultBox = document.getElementById("resultBox"),
+  var resultBox = document.getElementById("resultBox"),
       shot = document.getElementById("shot").value.split(','),
       winn = shot[Math.floor(Math.random()*shot.length)];
 
-  resultBox.innerHTML = " ";
-  resultBox.innerHTML += "<p>" + winn + "</p>";
+  if(shot.length > 2) {
+    resultBox.innerHTML = " ";
+    resultBox.innerHTML += "<p>" + winn + "</p>";
+  } else if(shot[0] == ""){
+    resultBox.innerHTML = "<p>Você esqueceu de digitar os valores. ;)</p>";
+  } else {
+    resultBox.innerHTML = "<p>Coloque mais de um valor separado por vírgulas.</p>";
+  };
 };
 
 var sendToRaffle = document.getElementById("sendToRaffle");
